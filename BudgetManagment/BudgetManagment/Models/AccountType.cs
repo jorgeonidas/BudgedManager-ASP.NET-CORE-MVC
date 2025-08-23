04f36428ad6385bf4ddd134045234e10cc13ec00
Jorgeonidas <zangetsujm@gmail.com>
2025-08-23 13:16:40 -0300

    validations by attribute

--- BudgetManagment/BudgetManagment/Models/AccountType.cs
+++ BudgetManagment/BudgetManagment/Models/AccountType.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using BudgetManagment.Validations;
+using System.ComponentModel.DataAnnotations;
 
 namespace BudgetManagment.Models
 {
@@ -7,22 +8,10 @@ namespace BudgetManagment.Models
         public int Id { get; set; }
         //custom error messages
         [Required(ErrorMessage = "{0} is required")]
-        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "{0} must be between {2} and {1} characters")]
         [Display(Name = "Name of the account type")]// Display attribute to change the name of the field in the validation message
+        [FirstCharacterCapitalized]//our custom validation attribute
         public string Name { get; set; }
         public int UserId { get; set; }
         public int Orden { get; set; }
-
-        /*Other validation types*/
-        [Required(ErrorMessage = "{0} is required")]
-        [EmailAddress(ErrorMessage = "The field {0} is not a valid email address")]
-        public string Email { get; set; }
-        [Range(minimum: 18, maximum: 130, ErrorMessage = "The field {0} must be between {1} and {2}")]
-        public int Age { get; set; }
-        [Url(ErrorMessage = "The field {0} must be a valid URL")]
-        public string Url { get; set; }
-        [CreditCard(ErrorMessage = "The field {0} must be a valid credit card number")]
-        [Display(Name = "Credit Card")]
-        public string CreditCard { get; set; }
     }
 }
---
+++ BudgetManagment/BudgetManagment/Validations/FirstCharacterCapitalizedAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetManagment.Validations
+{
+    public class FirstCharacterCapitalizedAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // If the value is null or empty, we consider it valid (use [Required] for mandatory fields)
+            if (value == null || string.IsNullOrEmpty(value.ToString()))
+            {
+                return ValidationResult.Success;
+            }
+
+            var firstCharacter = value.ToString()[0].ToString();
+            if(firstCharacter != firstCharacter.ToUpper())
+            {
+                return new ValidationResult("The first character must be uppercase");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
